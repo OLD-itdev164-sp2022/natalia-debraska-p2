00e663b5ac35076ea149a26ef5baac99e918be57
ndebraska <castron1@gmatc.matc.edu>
2022-05-02 16:06:47 -0500

    Completed Fase 3

--- blog/src/components/themes/Acai/Acai.js
+++ blog/src/components/themes/Acai/Acai.js
@@ -1,5 +1,30 @@
+import { withTheme } from "styled-components"
+
 const theme = {
-    header: {backgroundColor: '#46295A'}
+    space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+    colors: {
+        acai: {
+            0: '#46295A',
+            1: '#9400d3',
+            2: '#993399'
+        }
+    } 
 }
 
-export { theme as Acai }
+const variants = {
+    iconButton: {
+        primary: {
+            color: theme.colors.acai[0]
+        },
+        contrast: {
+            color: theme.colors.acai[2]
+        }
+    },
+    header: {
+        primary: {
+            backgroundColor: theme.colors.acai[0]
+        }
+    }
+}
+
+export const Acai = { ...theme, variants}
