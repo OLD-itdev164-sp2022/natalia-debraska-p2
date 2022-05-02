74ad85090720db05a9fbece45220565075005a3d
ndebraska <castron1@gmatc.matc.edu>
2022-05-02 00:08:56 -0500

    Completed Fase 1

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,15 +4,15 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-//import * as styles from "../components/index.module.css"
+import { List, ListItem } from "../components/List"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <ul>
+    <List width={[1, 1/2, 2/3]} p={2}>
     {
       data.allContentfulBlogPost.edges.map(edge => (
-        <li>
+        <ListItem p={3}>
           <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
           <div>
             <GatsbyImage
@@ -23,10 +23,10 @@ const IndexPage = ({ data }) => (
           <div>
             {edge.node.body.childMarkdownRemark.excerpt}
           </div>
-        </li>
+        </ListItem>
       ))
     }
-    </ul>
+    </List>
   </Layout>
 )
 
@@ -49,7 +49,7 @@ export const query = graphql`
             gatsbyImageData(
               layout: CONSTRAINED
               placeholder: BLURRED
-              width: 300
+              width: 600
             )
           }
         }
