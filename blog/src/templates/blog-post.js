46fc6a7a9051ff649fc87946097975871207730d
ndebraska <castron1@gmatc.matc.edu>
2022-05-04 22:51:34 -0500

    Finished

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { Layout } from '../components/layout';
-import { H1 } from '../components/Heading'
+import { GatsbyImage } from "gatsby-plugin-image"
+import Layout from '../components/layout';
+import { H1 } from '../components/Heading';
 
 const BlogPost = ({ data }) => {
-    const { title,body } = data.contentfulBlogPost;
+    const { title,body, heroImage } = data.contentfulBlogPost;
 
     return (
         <Layout>
+            <GatsbyImage
+                image={heroImage.gatsbyImageData}
+                alt="heroImage"
+            />
             <H1>{title}</H1>
             <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
         </Layout>
@@ -18,14 +23,21 @@ export default BlogPost;
 
 export const pageQuery = graphql`
     query blogPostQuery($slug: String!) {
-	    contentfulBlogPost(slug: {eq: $slug}) {
-		    title
-            slug
-            body {
+        contentfulBlogPost(slug: {eq: $slug}) {
+    	    title
+    	    slug
+    	    body {
 				childMarkdownRemark {
 					html
                 }
             }
-  		}
+    	    heroImage {
+                gatsbyImageData(
+                    layout: CONSTRAINED
+                    placeholder: BLURRED
+                    width: 600
+                )
+            }
+        }
     }
 `
